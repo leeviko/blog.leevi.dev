adcd3449902f4331148510b296647754af10f164
leeviko <leevikotilaine@gmail.com>
2022-09-11 17:15:18 +0300

    Button loading

---
+++ client/src/components/LoaderInline.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "../styles/loader.css";
+
+const LoaderInline = () => {
+  return (
+    <div className="inline-loader-container">
+      <div className="inline-loader">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default LoaderInline;
