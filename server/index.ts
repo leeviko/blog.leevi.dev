6e49d3936ad79e39f03b8a6af8c2edd56b41c802
leeviko <leevikotilaine@gmail.com>
2022-10-04 12:36:16 +0300

    Reduce fingerprinting

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,4 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, NextFunction, Request, Response } from "express";
 import session from "express-session";
 import Redis from "ioredis";
 import connectRedis from "connect-redis";
@@ -11,6 +11,7 @@ const port = process.env.PORT || 5000;
 
 const app: Express = express();
 
+app.disable("x-powered-by");
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -41,6 +42,15 @@ app.use("/api", apiLimiter);
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/posts", require("./routes/api/posts"));
 app.use("/api/auth", require("./routes/api/auth"));
+
+app.use((req, res, next) => {
+  res.status(404).send({ msg: "Couldnt find the resource" });
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack);
+  res.status(500).send({ msg: "Something went wrong" });
+});
 
 app.listen(port, () => {
   console.log(`⚡️ Server is running on port ${port}`);
